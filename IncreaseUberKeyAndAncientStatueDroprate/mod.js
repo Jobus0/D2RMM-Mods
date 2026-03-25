ac02356099ec54dbd6e2b2591ae3c0f812b4666d
Jobus <johan.tylon@gmail.com>
2026-03-26 00:08:17 +0100

    Fix ancient statues using key multiplier instead of statue multiplier

--- IncreaseUberKeyAndAncientStatueDroprate/mod.js
+++ IncreaseUberKeyAndAncientStatueDroprate/mod.js
@@ -3,25 +3,25 @@ treasureclassexFilenames.forEach((treasureclassexFilename) => {
   const treasureclassex = D2RMM.readTsv(treasureclassexFilename);
   if (!treasureclassex || treasureclassex.rows.length === 0) return;
 
-  function MultiplyProb(row, item) {
+  function MultiplyProb(row, item, mult) {
     for (let i = 1; i <= 10; i++) {
       if (row['Item' + i] === item) {
-        row['Prob' + i] = Math.floor(row['Prob' + i] * config.multiplier);
+        row['Prob' + i] = Math.floor(row['Prob' + i] * mult);
         return;
       }
     }
   }
 
   treasureclassex.rows.forEach((row) => {
-    MultiplyProb(row, 'pk1');
-    MultiplyProb(row, 'pk2');
-    MultiplyProb(row, 'pk3');
+    MultiplyProb(row, 'pk1', config.multiplier);
+    MultiplyProb(row, 'pk2', config.multiplier);
+    MultiplyProb(row, 'pk3', config.multiplier);
 
-    MultiplyProb(row, 'Ancient Statue 1');
-    MultiplyProb(row, 'Ancient Statue 2');
-    MultiplyProb(row, 'Ancient Statue 3');
-    MultiplyProb(row, 'Ancient Statue 4');
-    MultiplyProb(row, 'Ancient Statue 5');
+    MultiplyProb(row, 'Ancient Statue 1', config["statue-multiplier"]);
+    MultiplyProb(row, 'Ancient Statue 2', config["statue-multiplier"]);
+    MultiplyProb(row, 'Ancient Statue 3', config["statue-multiplier"]);
+    MultiplyProb(row, 'Ancient Statue 4', config["statue-multiplier"]);
+    MultiplyProb(row, 'Ancient Statue 5', config["statue-multiplier"]);
   });
   D2RMM.writeTsv(treasureclassexFilename, treasureclassex);
 });
